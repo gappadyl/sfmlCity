d28f9ca1c4dd51fc925bc4dc864bac6ea4418ce0
RobertBowenCode <49251520+RobertBowenCode@users.noreply.github.com>
2019-12-02 12:46:48 -0800

    12/2/19 update triangle VertexArray

--- sfmlCity/City.cpp
+++ sfmlCity/City.cpp
@@ -4,14 +4,20 @@
 //inilization func
 void City::initWindow()
 {
-	this->window = new sf::RenderWindow(sf::VideoMode(500, 500), "test Window", sf::Style::Default);
+	this->window = new sf::RenderWindow(sf::VideoMode(500, 500), "test Window", sf::Style::Close | sf::Style::Resize);
 }
 
-
+void City::initTriangle()
+{
+	this->triangle = new sf::VertexArray(sf::Triangles, 3);
+	
+}
 //constructor/destructor
 City::City()
-{
+{ 
 	this->initWindow(); 
+	this->initTriangle(); 
+
 }
 
 City::~City()
@@ -28,7 +34,7 @@ void City::updateSFMLEvents()
 {
 	while (this->window->pollEvent(this->sfEvent))
 	{
-		if (sfEvent.type == sf::Event::Closed)
+		if (this->sfEvent.type == sf::Event::Closed)
 		{
 			this->window->close(); 
 	    }
@@ -44,7 +50,7 @@ void City::update()
 void City::render()
 {
 	//clears the window
-	this->window->clear;
+	this->window->clear();
 	//displays the window
 	this->window->display(); 
 }
